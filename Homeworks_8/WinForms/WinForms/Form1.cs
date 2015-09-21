61cd28ee86288e45c16de0d824cebb06671a4ddf
AlexChipev <alexchipev@abv.bg>
2015-09-21 17:56:08 +0300

    Homeworks_8

---
+++ Homeworks_8/WinForms/WinForms/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter text = new StreamWriter
+                  ("D:\\C-Homeworks\\Homeworks_8\\WinForms\\WinForms\\Info.txt", true))
+            {
+                text.WriteLine("User details: ");
+                string[] write = new string[] 
+                { (label1.Text + ": " + textBox1.Text),
+                  (label2.Text + ": " + textBox2.Text), 
+                  (label3.Text + ": " + textBox3.Text) };
+
+                foreach (var item in write)
+                {
+                    text.WriteLine(item);
+                }
+                text.WriteLine();
+            }
+        }
+    }
+}
